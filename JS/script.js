df737fff0be450f3f5e28377f94c230ba96c2689
Thiago Barreto De Oliveira <91765675+BOThiago@users.noreply.github.com>
2022-10-09 13:45:17 -0300

    Front

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,15 @@
 const inputs = document.querySelectorAll('.input');
 
 const handleFocus = ({ target }) => {
-    const span = target.nextElementSibling;
-    span.classList.add('span-active');
-  }
+  const span = target.previousElementSibling;
+  span.classList.add('span-active');
+}
 
 const handleFocusOut = ({ target }) => {
     if (target.value === '') {
-      const span = target.nextElementSibling;
-      span.classList.remove('span-active');
-    }
+    const span = target.previousElementSibling;
+    span.classList.remove('span-active');
+  }
 }
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
---
+++ README.md
@@ -0,0 +1,7 @@
+echo "Front Formul-rio" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/BOThiago/Formul-rio.git
+git push -u origin main
